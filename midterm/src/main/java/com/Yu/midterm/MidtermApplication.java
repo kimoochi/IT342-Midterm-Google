1ce0299e08300301fae69d9746b100adb8c1241d
kimoochi <kimyu4606@gmail.com>
2025-03-05 02:59:52 +0800

    Refactored to use .env for secrets

--- midterm/src/main/java/com/Yu/midterm/MidtermApplication.java
+++ midterm/src/main/java/com/Yu/midterm/MidtermApplication.java
@@ -1,5 +1,6 @@
 package com.Yu.midterm;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MidtermApplication {
 
 	public static void main(String[] args) {
+		// Load the .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Optionally set system properties (Spring Boot can access env vars directly too)
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
 		SpringApplication.run(MidtermApplication.class, args);
 	}
-
 }
